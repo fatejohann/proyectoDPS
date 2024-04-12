198e3e1de7fd22c3db675cbe6a453f892ecd1322
Julian Moreno <juliancastillo204@gmail.com>
2024-04-12 11:27:39 -0600

    Screen Login

--- portal_Educativo/App.js
+++ portal_Educativo/App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {createNativeStackNavigator} from'@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Login from './src/Screens/Login';
+import { useState, useEffect } from 'react';
+
+//TIPOGRAFIA
+import * as Font from 'expo-font'
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  //Nos servira para exportar nuestra fuente
+  const [fontsLoaded, setFontsload] = useState(false);
+
+  useEffect(()=>{
+    if(!fontsLoaded){
+      loadFonts()
+    }
+  });
+  const loadFonts= async()=>{
+    await Font.loadAsync({
+      'JockeyOne': require('./src/utils/fonts/JockeyOne-Regular.ttf')
+     
+    })
+    
+  setFontsload(true);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Se vienen Cositas </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' >
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>  
   );
 }
 

10d45e7be2aca22f8fb3720dbbe9939987d4d404
Julian Moreno <juliancastillo204@gmail.com>
2024-05-07 21:50:05 -0600

    Solucionando bug

--- portal_Educativo/App.js
+++ portal_Educativo/App.js
@@ -42,7 +42,7 @@ export default function App() {
   
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' >
+      <Stack.Navigator initialRouteName='Login' >
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
         <Stack.Screen name="Registro" component={Registro}  options={{ headerShown: false }}/> 
 
